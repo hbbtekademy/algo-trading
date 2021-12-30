2edbc432dc4e788211986d9a290048f3f984be7c
hbb <acdc192002@yahoo.com>
2021-12-30 21:44:38 +0530

    fix typo

--- pkg/utils/env_utils.go
+++ pkg/utils/env_utils.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MustGetEnv(envVar string) string {
-	value, isSet := os.LookupEnv("envVar")
+	value, isSet := os.LookupEnv(envVar)
 	if !isSet || strings.TrimSpace(value) == "" {
 		log.Fatalf("Mandatory environment variable %s not set...", envVar)
 	}
