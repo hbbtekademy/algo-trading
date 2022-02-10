86b14886384add593493b8c61c960b95a2a09046
hbb <acdc192002@yahoo.com>
2022-02-10 16:35:04 +0530

    function to downsample 1 min df to 15 min df

--- python/ChartBusters/helpers.py
+++ python/ChartBusters/helpers.py
@@ -19,3 +19,23 @@ def get_hourly_df(df: pd.DataFrame, offset: str = '15Min') -> pd.DataFrame:
     df_60min.dropna(subset=['Open'], inplace=True)
 
     return df_60min
+
+
+def get_15min_df(df: pd.DataFrame, offset: str = '0Min') -> pd.DataFrame:
+    df_temp = df[:]
+    df_15min_o = df_temp['Open'].resample(
+        '15Min', offset=offset).apply({'Open': 'first'})
+    df_15min_h = df_temp['High'].resample(
+        '15Min', offset=offset).apply({'High': 'max'})
+    df_15min_l = df_temp['Low'].resample(
+        '15Min', offset=offset).apply({'Low': 'min'})
+    df_15min_c = df_temp['Close'].resample(
+        '15Min', offset=offset).apply({'Close': 'last'})
+    df_15min_vol = df_temp['Volume'].resample(
+        '15Min', offset=offset).apply({'Volume': 'sum'})
+
+    df_15min = pd.concat([df_15min_o, df_15min_h, df_15min_l,
+                         df_15min_c, df_15min_vol], axis=1)
+    df_15min.dropna(subset=['Open'], inplace=True)
+
+    return df_15min
