cbc5e1faf923f486071a11813a98e53c952b28b7
hbb <acdc192002@yahoo.com>
2022-02-10 16:36:27 +0530

    V3 refactoring for cloud deployment

---
+++ python/sti_runner_v3.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from ChartBusters.cb_chart import CBChart
+from ChartBusters.Strategy.cb_supertrend_strategy_v3 import CBSuperTrendStrategyV3
+from ChartBusters.cb_signal_v3 import CBSignal
+from ChartBusters import helpers
+from typing import List
+
+# file = './python/BackTest/config/STI_Nifty_BackTest_2021.csv'
+# file = './python/BackTest/config/STI_Nifty_BackTest_2020.csv'
+# file = './python/BackTest/config/STI_Nifty_BackTest_2019.csv'
+# file = './python/BackTest/config/STI_Nifty_BackTest_2018.csv'
+# file = './python/BackTest/config/STI_Nifty_BackTest_2017.csv'
+file = './python/BackTest/config/STI_NiftyFut_Verify.csv'
+
+# file = './python/BackTest/config/STI_BankNifty_BackTest_2021.csv'
+# file = './python/BackTest/config/STI_BankNifty_BackTest_2020.csv'
+# file = './python/BackTest/config/STI_BankNifty_BackTest_2019.csv'
+# file = './python/BackTest/config/STI_BankNifty_BackTest_2018.csv'
+# file = './python/BackTest/config/STI_BankNiftyFut_Verify.csv'
+
+ema_interval = 31
+supertrend_ema_margin = 30
+stoploss_gap = 20
+close_ema_margin = 35000000
+
+input_df = pd.read_csv(file, parse_dates=['Start', 'End'], index_col=['Sym'])
+
+all_signals = list()
+for index, row in input_df.iterrows():
+    file = './python/BackTest/Hist15min/' + index + '-HIST-15M.csv'
+    df = pd.read_csv(file, parse_dates=['Date'], index_col=['Date'])
+    df_60min = helpers.get_hourly_df(df)
+    chart = CBChart(index, int(row['LotSize']), df, ema_interval=ema_interval)
+
+    strategy = CBSuperTrendStrategyV3(
+        index, chart, row['Expiry'], stoploss_margin=int(row['StopLoss15']), stoploss_gap=stoploss_gap,
+        supertrend_ema_margin=supertrend_ema_margin, close_ema_margin=close_ema_margin)
+
+    strategy.back_test(row['Start'].tz_localize(
+        'Asia/Kolkata'), row['End'].tz_localize('Asia/Kolkata'))
