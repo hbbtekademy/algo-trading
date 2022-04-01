cc9a218f34bb3bb489ce25e7b3e66c222ef49e7e
hbb <acdc192002@yahoo.com>
2022-04-01 17:12:33 +0530

    switch to Apr series

--- python/sti_plotter.py
+++ python/sti_plotter.py
@@ -1,17 +1,18 @@
 import pandas as pd
-import pandas_ta as ta
-from ChartBusters import helpers
 from ChartBusters.cb_chart import CBChart
 from ChartBusters.Plot.cb_supertrend_plot import CBSuperTrendPlot
 
-file = './python/BackTest/Hist15min/NIFTY22FEBFUT-HIST-15M.csv'
+# file = './python/BackTest/Hist15min/NIFTY22FEBFUT-HIST-15M.csv'
+# file = './python/BackTest/Hist15min/NIFTY22MARFUT-HIST-15M.csv'
+file = './python/BackTest/Hist15min/NIFTY22APRFUT-HIST-15M.csv'
 # file = './python/BackTest/Hist15min/NIFTY-HIST-15M.csv'
 sym = 'NIFTY'
-lot_size = 75
+lot_size = 50
 df = pd.read_csv(file, parse_dates=['Date'], index_col=['Date'])
 
-chart15 = CBChart(sym, lot_size, df, ema_interval=31, sma_interval=29, sti_interval=11, sti_multiplier=2)
+chart15 = CBChart(sym, lot_size, df, ema_interval=31,
+                  sma_interval=29, sti_interval=11, sti_multiplier=2)
 
 sti_plot = CBSuperTrendPlot(chart15)
 
-sti_plot.plot('2022-02-15', '2022-02-17')
+sti_plot.plot('2022-03-25', '2022-04-15')
