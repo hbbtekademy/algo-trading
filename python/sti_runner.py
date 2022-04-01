39f922ea1e89ed15a29990c847ac2f5eb5418d36
hbb <acdc192002@yahoo.com>
2022-04-01 17:12:16 +0530

    remove hardcoded params

--- python/sti_runner.py
+++ python/sti_runner.py
@@ -27,6 +27,8 @@ sma_interval = 29
 MA = constants.EMA
 supertrend_ma_margin = 50
 stoploss_gap = 20
+sti_interval = 11
+sti_multiplier = 2
 # close_ema_margin = 2500000000
 
 input_df = pd.read_csv(file, parse_dates=['Start', 'End'], index_col=['Sym'])
@@ -38,37 +40,39 @@ for index, row in input_df.iterrows():
     df = pd.read_csv(file, parse_dates=['Date'], index_col=['Date'])
     df_60min = helpers.get_hourly_df(df)
     chart = CBChart(index, int(
-        row['LotSize']), df, ema_interval=ema_interval, sma_interval=sma_interval, MA=MA, sti_interval=11, sti_multiplier=2)
+        row['LotSize']), df, ema_interval=ema_interval, sma_interval=sma_interval, MA=MA, sti_interval=sti_interval, sti_multiplier=sti_multiplier)
     chart60 = CBChart(index, int(row['LotSize']),
-                      df_60min, ema_interval=ema_interval, sma_interval=sma_interval, MA=MA, sti_interval=11, sti_multiplier=2)
+                      df_60min, ema_interval=ema_interval, sma_interval=sma_interval, MA=MA, sti_interval=sti_interval, sti_multiplier=sti_multiplier)
 
-    backtest=CBSuperTrendBackTest(
+    backtest = CBSuperTrendBackTest(
         chart, chart60, row['Expiry'],
-        stoploss_margin15 = int(row['StopLoss15']), stoploss_margin60 = int(row['StopLoss60']),
-        supertrend_ma_margin = supertrend_ma_margin, stoploss_gap = stoploss_gap)
+        stoploss_margin15=int(row['StopLoss15']), stoploss_margin60=int(row['StopLoss60']),
+        supertrend_ma_margin=supertrend_ma_margin, stoploss_gap=stoploss_gap)
 
-    signals15, signals60=backtest.back_test(row['Start'].tz_localize(
+    # print(df.tail(50))
+
+    signals15, signals60 = backtest.back_test(row['Start'].tz_localize(
         'Asia/Kolkata'), row['End'].tz_localize('Asia/Kolkata'))
 
-    total_monthly_pnl=0
+    total_monthly_pnl = 0
     for s in signals15:
-        total_monthly_pnl=total_monthly_pnl + s.pnl
+        total_monthly_pnl = total_monthly_pnl + s.pnl
         all_signals.append(s)
 
-    total_monthly_pnl60=0
+    total_monthly_pnl60 = 0
     for s in signals60:
-        total_monthly_pnl60=total_monthly_pnl60 + s.pnl
+        total_monthly_pnl60 = total_monthly_pnl60 + s.pnl
         all_signals60.append(s)
 
     print('{}'.format(total_monthly_pnl))
     # print('Monthly 60Min Pnl,{}'.format(total_monthly_pnl60))
 
-total_pnl=0
-total_count=0
+total_pnl = 0
+total_count = 0
 CBSignal.print_header()
 for signal in all_signals:
-    total_pnl=total_pnl + signal.pnl
-    total_count=total_count + 1
+    total_pnl = total_pnl + signal.pnl
+    total_count = total_count + 1
     signal.pretty_print()
 
 print(" ")
