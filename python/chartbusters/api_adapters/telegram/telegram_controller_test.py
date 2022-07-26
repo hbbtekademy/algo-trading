c44b5269a14594a8d67a63e1ccb15a6f5b29a4a1
Shirish <53242367+1Shirish@users.noreply.github.com>
2022-07-26 14:30:12 +0530

    Revert "Telegram integration to send message to Telegram Channel"
    This reverts commit acb3500b889f2b984365c537b3051486f7bb3623.

--- python/chartbusters/api_adapters/telegram/telegram_controller_test.py
+++
@@ -1,6 +0,0 @@
-from python.chartbusters.api_adapters.telegram import telegram_controller
-
-telegram_controller.send_message('TEST: Buy Reliance 2500 CE at 34.30')
-telegram_controller.send_message('TEST: Book 50 % profits in Nifty 16100 CE at 177')
-telegram_controller.send_message('TEST: BTST Hindalco 375 CE at 6')
-telegram_controller.send_message('TEST: Revise SL for Bajfinance 6100 CE at 93.90')
