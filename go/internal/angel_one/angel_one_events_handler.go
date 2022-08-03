46141a659b1de0413ff763a378b57d35f31ac329
Shirish <53242367+1Shirish@users.noreply.github.com>
2022-08-03 23:13:29 +0530

    Angel One event handlers

--- go/internal/angel_one/angel_one_events_handler.go
+++ go/internal/angel_one/angel_one_events_handler.go
@@ -1,10 +1,14 @@
 package angel_one
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/angelbroking-github/smartapigo/websocket"
+	"github.com/go-redis/redis/v8"
 	"log"
 	"org.hbb/algo-trading/go/models"
+	redisutils "org.hbb/algo-trading/go/pkg/utils/redis"
+	"os"
 	"time"
 )
 
@@ -17,7 +21,10 @@ var (
 )
 
 //Observed market tick data is received after market close. Buffer time need to collate these ticks.
-const bufferTimeToCollectAfterMarketHoursTickData = -5
+const (
+	bufferTimeToCollectAfterMarketHoursTickData = -5
+	channelSize                                 = 5000
+)
 
 // Triggered when a message is received
 func onMessage(message []map[string]interface{}) {
@@ -84,6 +91,89 @@ func onConnect() {
 	err := socketClient.Subscribe()
 	if err != nil {
 		fmt.Println("err: ", err)
+	}
+	channelClosed = false
+
+	fileTickChannel = make(chan *models.Tick, channelSize)
+	redisTickChannel = make(chan *models.Tick, channelSize)
+
+	go streamTicksToFile()
+	go streamTicksToRedisDatabase()
+
+}
+
+func streamTicksToRedisDatabase() {
+	defer func(redisClient *redis.Client) {
+		err := redisClient.Close()
+		if err != nil {
+			log.Fatalln("Error closing redis client: ", err)
+		}
+	}(redisClient)
+	for {
+		tick, ok := <-redisTickChannel
+		if !ok {
+			log.Printf("Redis Tick Channel closed. Exiting streamTicksToRedisDatabase goroutine...")
+			return
+		}
+		redisutils.WriteTickToRedis(ctx, redisClient, tick)
+	}
+}
+
+func createTickFile() (*os.File, error) {
+	var err error
+	y, m, d := time.Now().Date()
+	fn := fmt.Sprintf("Ticker-%d%d%d.csv", y, int(m), d)
+
+	tickDataFile, err = os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Error opening zerodha file:", err)
+	}
+
+	log.Printf("Tick file %s created...", fn)
+
+	return tickDataFile, err
+}
+
+func streamTicksToFile() {
+	f, err := createTickFile()
+	if err != nil {
+		log.Fatalln("Error creating zerodha file: ", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatalln("Error closing zerodha file: ", err)
+		}
+	}(f)
+
+	for {
+		tick, ok := <-fileTickChannel
+		if !ok {
+			log.Printf("File Tick Channel closed. Exiting streamTicksToFile goroutine...")
+			return
+		}
+		writeTickToCsvFile(tickDataFile, tick)
+	}
+}
+
+func writeTickToCsvFile(f *os.File, tick *models.Tick) {
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	err := w.Write(getTickData(tick))
+	if err != nil {
+		log.Fatalln("Error writing to zerodha file:", err)
+	}
+}
+
+func getTickData(tick *models.Tick) []string {
+	return []string{
+		instruments[tick.InstrumentToken].Sym,
+		tick.ExchangeTS.Format(time.RFC3339),
+		tick.LastTradeTS.Format(time.RFC3339),
+		fmt.Sprintf("%f", tick.LTP),
+		fmt.Sprintf("%d", tick.LastTradedQuantity),
+		fmt.Sprintf("%d", tick.VolumeTraded),
 	}
 }
 
